bf791d627395e4c6375a23e62b7718cc36a816bd
GalfiZsolt <galfi_zsolt@yahoo.com>
2019-02-10 21:44:43 +0200

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,7 +14,7 @@ function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
-    menuNav.classList.add("close");
+    menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
 
@@ -23,8 +23,11 @@ function toggleMenu() {
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
-    menuNav.classList.remove("close");
+    menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.add("show"));
+    navItems.forEach(item => item.classList.remove("show"));
+
+    //Set Menu State
+    showMenu = false;
   }
 }
